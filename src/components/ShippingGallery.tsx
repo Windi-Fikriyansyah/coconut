56c8ec1d187ec74049aa0aa1151143bc2f425ce1
windi fikriyansyah <diwin321@gmail.com>
2026-02-12 15:26:02 +0700

    dsd

--- src/components/ShippingGallery.tsx
+++ src/components/ShippingGallery.tsx
@@ -108,6 +108,7 @@ const ShippingGallery = ({ data, section }: ShippingGalleryProps) => {
                                         src={item.src || ''}
                                         alt={item.title || 'Shipping Documentation'}
                                         fill
+                                        unoptimized
                                         className="object-cover transition-transform duration-700 group-hover:scale-110"
                                         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
                                     />
